70e2789bab4c9095b98e92e483b6185ac1257224
Steve Wilcox <steve.wilcox@outlook.com.au>
2019-01-28 18:43:43 +1100

    Added vscode settings to the project. Updated components to have consistent use of semicolins and double quotes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: [ 'standard', 'standard-react' ],
-  plugins: [ 'react' ],
+  parser: "babel-eslint",
+  extends: ["standard", "standard-react"],
+  plugins: ["react"],
   rules: {
-    'jsx-quotes': [ 'error', 'prefer-double' ]
+    "jsx-quotes": ["error", "prefer-double"],
+    quotes: [2, "double"],
+    semi: 0
   }
-}
+};
--- src/Rect/index.js
+++ src/Rect/index.js
@@ -1,18 +1,18 @@
-import PropTypes from 'prop-types'
-import React, { PureComponent } from 'react'
-import { getLength, getAngle, getCursor } from '../utils'
-import StyledRect from './StyledRect'
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { getLength, getAngle, getCursor } from "../utils";
+import StyledRect from "./StyledRect";
 
 const zoomableMap = {
-  'n': 't',
-  's': 'b',
-  'e': 'r',
-  'w': 'l',
-  'ne': 'tr',
-  'nw': 'tl',
-  'se': 'br',
-  'sw': 'bl'
-}
+  n: "t",
+  s: "b",
+  e: "r",
+  w: "l",
+  ne: "tr",
+  nw: "tl",
+  se: "br",
+  sw: "bl"
+};
 
 export default class Rect extends PureComponent {
   static propTypes = {
@@ -34,113 +34,125 @@ export default class Rect extends PureComponent {
     rotatable: PropTypes.bool,
     styles: PropTypes.object,
     zoomable: PropTypes.string
-  }
+  };
 
   static defaultProps = {
-    className: ''
-  }
+    className: ""
+  };
 
-  setElementRef = (ref) => { this.$element = ref }
+  setElementRef = ref => {
+    this.$element = ref;
+  };
 
   // Drag
-  startDrag = (e) => {
-    let { clientX: startX, clientY: startY } = e
-    this.props.onDragStart && this.props.onDragStart()
-    this._isMouseDown = true
-    const onMove = (e) => {
-      if (!this._isMouseDown) return // patch: fix windows press win key during mouseup issue
-      e.stopImmediatePropagation()
-      const { clientX, clientY } = e
-      const deltaX = clientX - startX
-      const deltaY = clientY - startY
-      this.props.onDrag(deltaX, deltaY)
-      startX = clientX
-      startY = clientY
-    }
+  startDrag = e => {
+    let { clientX: startX, clientY: startY } = e;
+    this.props.onDragStart && this.props.onDragStart();
+    this._isMouseDown = true;
+    const onMove = e => {
+      if (!this._isMouseDown) return; // patch: fix windows press win key during mouseup issue
+      e.stopImmediatePropagation();
+      const { clientX, clientY } = e;
+      const deltaX = clientX - startX;
+      const deltaY = clientY - startY;
+      this.props.onDrag(deltaX, deltaY);
+      startX = clientX;
+      startY = clientY;
+    };
     const onUp = () => {
-      document.removeEventListener('mousemove', onMove)
-      document.removeEventListener('mouseup', onUp)
-      if (!this._isMouseDown) return
-      this._isMouseDown = false
-      this.props.onDragEnd && this.props.onDragEnd()
-    }
-    document.addEventListener('mousemove', onMove)
-    document.addEventListener('mouseup', onUp)
-  }
+      document.removeEventListener("mousemove", onMove);
+      document.removeEventListener("mouseup", onUp);
+      if (!this._isMouseDown) return;
+      this._isMouseDown = false;
+      this.props.onDragEnd && this.props.onDragEnd();
+    };
+    document.addEventListener("mousemove", onMove);
+    document.addEventListener("mouseup", onUp);
+  };
 
   // Rotate
-  startRotate = (e) => {
-    if (e.button !== 0) return
-    const { clientX, clientY } = e
-    const { styles: { transform: { rotateAngle: startAngle } } } = this.props
-    const rect = this.$element.getBoundingClientRect()
+  startRotate = e => {
+    if (e.button !== 0) return;
+    const { clientX, clientY } = e;
+    const {
+      styles: {
+        transform: { rotateAngle: startAngle }
+      }
+    } = this.props;
+    const rect = this.$element.getBoundingClientRect();
     const center = {
       x: rect.left + rect.width / 2,
       y: rect.top + rect.height / 2
-    }
+    };
     const startVector = {
       x: clientX - center.x,
       y: clientY - center.y
-    }
-    this.props.onRotateStart && this.props.onRotateStart()
-    this._isMouseDown = true
-    const onMove = (e) => {
-      if (!this._isMouseDown) return // patch: fix windows press win key during mouseup issue
-      e.stopImmediatePropagation()
-      const { clientX, clientY } = e
+    };
+    this.props.onRotateStart && this.props.onRotateStart();
+    this._isMouseDown = true;
+    const onMove = e => {
+      if (!this._isMouseDown) return; // patch: fix windows press win key during mouseup issue
+      e.stopImmediatePropagation();
+      const { clientX, clientY } = e;
       const rotateVector = {
         x: clientX - center.x,
         y: clientY - center.y
-      }
-      const angle = getAngle(startVector, rotateVector)
-      this.props.onRotate(angle, startAngle)
-    }
+      };
+      const angle = getAngle(startVector, rotateVector);
+      this.props.onRotate(angle, startAngle);
+    };
     const onUp = () => {
-      document.removeEventListener('mousemove', onMove)
-      document.removeEventListener('mouseup', onUp)
-      if (!this._isMouseDown) return
-      this._isMouseDown = false
-      this.props.onRotateEnd && this.props.onRotateEnd()
-    }
-    document.addEventListener('mousemove', onMove)
-    document.addEventListener('mouseup', onUp)
-  }
+      document.removeEventListener("mousemove", onMove);
+      document.removeEventListener("mouseup", onUp);
+      if (!this._isMouseDown) return;
+      this._isMouseDown = false;
+      this.props.onRotateEnd && this.props.onRotateEnd();
+    };
+    document.addEventListener("mousemove", onMove);
+    document.addEventListener("mouseup", onUp);
+  };
 
   // Resize
   startResize = (e, cursor) => {
-    if (e.button !== 0) return
-    document.body.style.cursor = cursor
-    const { styles: { position: { centerX, centerY }, size: { width, height }, transform: { rotateAngle } } } = this.props
-    const { clientX: startX, clientY: startY } = e
-    const rect = { width, height, centerX, centerY, rotateAngle }
-    const type = e.target.getAttribute('class').split(' ')[ 0 ]
-    this.props.onResizeStart && this.props.onResizeStart()
-    this._isMouseDown = true
-    const onMove = (e) => {
-      if (!this._isMouseDown) return // patch: fix windows press win key during mouseup issue
-      e.stopImmediatePropagation()
-      const { clientX, clientY } = e
-      const deltaX = clientX - startX
-      const deltaY = clientY - startY
-      const alpha = Math.atan2(deltaY, deltaX)
-      const deltaL = getLength(deltaX, deltaY)
-      const isShiftKey = e.shiftKey
-      this.props.onResize(deltaL, alpha, rect, type, isShiftKey)
-    }
+    if (e.button !== 0) return;
+    document.body.style.cursor = cursor;
+    const {
+      styles: {
+        position: { centerX, centerY },
+        size: { width, height },
+        transform: { rotateAngle }
+      }
+    } = this.props;
+    const { clientX: startX, clientY: startY } = e;
+    const rect = { width, height, centerX, centerY, rotateAngle };
+    const type = e.target.getAttribute("class").split(" ")[0];
+    this.props.onResizeStart && this.props.onResizeStart();
+    this._isMouseDown = true;
+    const onMove = e => {
+      if (!this._isMouseDown) return; // patch: fix windows press win key during mouseup issue
+      e.stopImmediatePropagation();
+      const { clientX, clientY } = e;
+      const deltaX = clientX - startX;
+      const deltaY = clientY - startY;
+      const alpha = Math.atan2(deltaY, deltaX);
+      const deltaL = getLength(deltaX, deltaY);
+      const isShiftKey = e.shiftKey;
+      this.props.onResize(deltaL, alpha, rect, type, isShiftKey);
+    };
 
     const onUp = () => {
-      document.body.style.cursor = 'auto'
-      document.removeEventListener('mousemove', onMove)
-      document.removeEventListener('mouseup', onUp)
-      if (!this._isMouseDown) return
-      this._isMouseDown = false
-      this.props.onResizeEnd && this.props.onResizeEnd()
-    }
-    document.addEventListener('mousemove', onMove)
-    document.addEventListener('mouseup', onUp)
-  }
+      document.body.style.cursor = "auto";
+      document.removeEventListener("mousemove", onMove);
+      document.removeEventListener("mouseup", onUp);
+      if (!this._isMouseDown) return;
+      this._isMouseDown = false;
+      this.props.onResizeEnd && this.props.onResizeEnd();
+    };
+    document.addEventListener("mousemove", onMove);
+    document.addEventListener("mouseup", onUp);
+  };
 
-  render () {
+  render = () => {
     const {
       children,
       className,
@@ -152,15 +164,18 @@ export default class Rect extends PureComponent {
       zoomable,
       rotatable,
       parentRotateAngle
-    } = this.props
+    } = this.props;
     const style = {
       width: Math.abs(width),
       height: Math.abs(height),
       transform: `rotate(${rotateAngle}deg)`,
       left: centerX - Math.abs(width) / 2,
       top: centerY - Math.abs(height) / 2
-    }
-    const direction = zoomable.split(',').map(d => d.trim()).filter(d => d) // TODO: may be speed up
+    };
+    const direction = zoomable
+      .split(",")
+      .map(d => d.trim())
+      .filter(d => d); // TODO: may be speed up
 
     return (
       <StyledRect
@@ -169,8 +184,7 @@ export default class Rect extends PureComponent {
         className={`rect single-resizer ${className}`}
         style={style}
       >
-        {
-          rotatable &&
+        {rotatable && (
           <div className="rotate" onMouseDown={this.startRotate}>
             <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
               <path
@@ -180,26 +194,28 @@ export default class Rect extends PureComponent {
               />
             </svg>
           </div>
-        }
+        )}
 
-        {
-          direction.map(d => {
-            const cursor = `${getCursor(rotateAngle + parentRotateAngle, d)}-resize`
-            return (
-              <div key={d} style={{ cursor }} className={`${zoomableMap[ d ]} resizable-handler`} onMouseDown={(e) => this.startResize(e, cursor)} />
-            )
-          })
-        }
+        {direction.map(d => {
+          const cursor = `${getCursor(
+            rotateAngle + parentRotateAngle,
+            d
+          )}-resize`;
+          return (
+            <div
+              key={d}
+              style={{ cursor }}
+              className={`${zoomableMap[d]} resizable-handler`}
+              onMouseDown={e => this.startResize(e, cursor)}
+            />
+          );
+        })}
 
-        {
-          direction.map(d => {
-            return (
-              <div key={d} className={`${zoomableMap[ d ]} square`} />
-            )
-          })
-        }
+        {direction.map(d => {
+          return <div key={d} className={`${zoomableMap[d]} square`} />;
+        })}
         {children}
       </StyledRect>
-    )
-  }
+    );
+  };
 }
